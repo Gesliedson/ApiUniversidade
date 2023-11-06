55586b0f8f9ab6271395a829f083c2a4f3bcb2c7
Gesliedson <Gesliedsonsilva120@gmail.com>
2023-11-06 07:45:26 -0300

    CÓDIGO FINALIZADO

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -5,10 +5,14 @@ using System.Threading.Tasks;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
 using apiUniversidade.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace apiUniversidade.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+
+
     public class AlunoController : Controller
 
     {
@@ -46,7 +50,48 @@ namespace apiUniversidade.Controllers
             _context.SaveChanges();
 
             return new CreatedAtRouteResult ("GetAluno", new {id = aluno.Id},aluno);
+            
+                    
+        }
 
+
+         [HttpGet("{id:int}", Name = "GetAluno")]
+        public ActionResult<Aluno> Get(int id)
+
+        {
+            var aluno = _context.Alunos.FirstOrDefault(p=>p.Id == id);
+            if(aluno is null)
+                return NotFound("Curso não encontrado");
+            return aluno;
+
+        }
+
+
+        [HttpPut("id:int")]
+
+        public ActionResult Put ( int id, Aluno aluno){
+            if(id != aluno.Id)
+                return BadRequest();
+            
+            _context.Entry(aluno).State = EntityState.Modified;
+            _context.SaveChanges(); 
+
+            return Ok (aluno);
+        }
+
+
+        [HttpDelete ("{id int}")] 
+
+         public ActionResult Delete(int id) {
+            var aluno= _context.Alunos.FirstOrDefault (P=> P.Id == id);
+
+            if (aluno is null) {
+
+                return NotFound();
+             }
+                _context.Alunos.Remove(aluno);
+                _context.SaveChanges();
+                return Ok (aluno);
         }
 
     }
--- Controllers/DisciplinaController.cs
+++ Controllers/DisciplinaController.cs
@@ -2,21 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiUniversidade.Context;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+
 
 namespace apiUniversidade.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
+
     public class DisciplinaController : Controller
+
     {
         private readonly ILogger<DisciplinaController> _logger;
 
-        public DisciplinaController(ILogger<DisciplinaController> logger)
+         private readonly apiUniversidadeContext _context;
+
+        public DisciplinaController(ILogger<DisciplinaController> logger, apiUniversidadeContext context)
+
+
         {
             _logger = logger;
+            _context = context;
+
         }
+
         [HttpGet(Name = "disciplinas")]
         public List<Disciplina> GetDisciplina(){
             List<Disciplina> Disciplinas = new List<Disciplina>();
@@ -42,5 +58,57 @@ namespace apiUniversidade.Controllers
 
             return Disciplinas;
         }
+
+        [HttpPost]
+        public ActionResult Post (Disciplina disciplina){
+
+            _context.Disciplinas.Add(disciplina);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult ("GetDisciplina", new {id = disciplina.Id},disciplina);
+            
+                    
+        }
+
+
+         [HttpGet("{id:int}", Name = "GetDisciplina")]
+        public ActionResult<Disciplina> Get(int id)
+
+        {
+            var disciplina = _context.Disciplinas.FirstOrDefault(p=>p.Id == id);
+            if(disciplina is null)
+                return NotFound("Disciplina não encontrado");
+            return disciplina;
+
+        }
+
+
+        [HttpPut("id:int")]
+
+        public ActionResult Put ( int id, Disciplina disciplina){
+            if(id != disciplina.Id)
+                return BadRequest();
+            
+            _context.Entry(disciplina).State = EntityState.Modified;
+            _context.SaveChanges(); 
+
+            return Ok (disciplina);
+        }
+
+
+        [HttpDelete ("{id int}")] 
+
+         public ActionResult Delete(int id) {
+            var disciplina= _context.Disciplinas.FirstOrDefault (P=> P.Id == id);
+
+            if (disciplina is null) {
+
+                return NotFound();
+             }
+                _context.Disciplinas.Remove(disciplina);
+                _context.SaveChanges();
+                return Ok (disciplina);
+        }
+
     }
 }
