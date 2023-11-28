185bc70829bab72f08c012766d80c8ddd6bfa5e9
Gesliedson <Gesliedsonsilva120@gmail.com>
2023-11-28 11:52:48 -0300

    adições

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 using apiUniversidade.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
 builder.Services.AddDbContext<apiUniversidadeContext>(options => options.UseNpgsql(connectionString));
 
+
 builder.Services.AddControllers();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<apiUniversidadeContext>()
+    .AddDefaultTokenProviders();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
